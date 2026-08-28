7611ce64c6743f989de11cb0dc61b32836e17287
45kikurage-rgb <45kikurage@gmail.com>
2026-08-28 18:30:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='the-fool-head-personal-v3.4.8';
+const CACHE='the-fool-head-personal-v3.4.9';
 const ASSETS=['./','./index.html','./manifest.json','./icon-any.png','./icon-maskable.png','./quest_icon_chest.png'];
 
 self.addEventListener('install',event=>{
